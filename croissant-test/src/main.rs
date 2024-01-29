ced84925b5bf6dacf1990e5531bd602439322f76
Bertik23 <bertikxxiii@gmail.com>
2024-01-29 10:40:39 +0100

    better tests

--- croissant-test/src/main.rs
+++ croissant-test/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveTime;
 use core::future::Future;
 use core::pin::Pin;
 use croissant::{croissant, Croissant};
@@ -19,14 +20,33 @@ async fn job3(c: C) {
     println!("Job3 {:?}", c)
 }
 
-#[tokio::main]
-async fn main() {
-    let mut c = Croissant::new();
-    c.add_job((), |_| println!("job1"));
-    c.add_async_job((), Box::new(job2_wrapper));
-    c.add_async_job((), Box::new(job3_croissant));
-    // c.run_every(Duration::from_secs(2));
-    // c.run_every(Duration::from_secs(1));
-    c.run_at(chrono::NaiveTime::from_hms(20, 49, 40));
+#[croissant]
+async fn hello(msg: String) {
+    println!("{}", msg);
+}
+
+fn main() {
+    let mut croissant = Croissant::new();
+    croissant
+        .add_async_job("Hello World".to_string(), Box::new(hello_croissant));
+    croissant.run_every(Duration::from_secs(5));
+    croissant.run_at(NaiveTime::from_hms(12, 0, 0));
     thread::sleep(Duration::from_secs(100));
 }
+// #[tokio::main]
+// async fn main() {
+//     // let mut c = Croissant::new();
+//     // c.add_job((), |_| println!("job1"));
+//     // c.add_async_job((), Box::new(job2_wrapper));
+//     // c.add_async_job((), Box::new(job3_croissant));
+//     // // c.run_every(Duration::from_secs(2));
+//     // // c.run_every(Duration::from_secs(1));
+//     // c.run_at(chrono::NaiveTime::from_hms(20, 49, 40));
+//     // thread::sleep(Duration::from_secs(100));
+//     let mut croissant = Croissant::new();
+//     croissant.add_async_job("Hello World", |msg: &str| async move {
+//         println!("{}", msg);
+//     });
+//     croissant.run_every(Duration::from_secs(5));
+//     croissant.run_at(NaiveTime::from_hms(12, 0, 0));
+// }
